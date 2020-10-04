8fd4c65b033a35da851bad3fc5465b80053ab08f
isabellagomez <ilgomezt@ucsd.edu>
2020-10-04 12:11:49 -0700

    Adding main

--- is_string_integer.py
+++ is_string_integer.py
@@ -6,3 +6,12 @@
 # as input and returns True or False if that character represents a valid
 # integer in base 10.
 
+
+##############################################################################
+# Will take an input of a single character string and return True if it is a #
+# base 10 integer.                                                           #
+##############################################################################
+def is_string_integer(input_string):
+    if input_string.isdigit():
+        print("true")
+        #return True
---
+++ main.py
@@ -0,0 +1,13 @@
+class Homework1:
+    def __init__(self):
+        pass
+
+    def is_string_integer(input_string):
+        if input_string:
+            print("true")
+
+if __name__ == '__main__':
+
+    my_Homework1 = Homework1()
+    my_Homework1.is_string_integer()
+
